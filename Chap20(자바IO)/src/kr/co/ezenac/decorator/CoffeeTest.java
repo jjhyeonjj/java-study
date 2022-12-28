d3a1834b4a8728f218e8433088d8191d8e986bd3
jjhyeonjj <jjh4523a@gmail.com>
2022-12-28 11:24:30 +0900

    Chap20.자바IO

---
+++ Chap20(자바IO)/src/kr/co/ezenac/decorator/CoffeeTest.java
@@ -0,0 +1,24 @@
+package kr.co.ezenac.decorator;
+
+public class CoffeeTest {
+
+	public static void main(String[] args) {
+		
+		Coffee kenyaAmericano = new KenyaAmericano();
+		kenyaAmericano.brewing();
+		System.out.println();
+		
+		Coffee kenyaLatte = new Latte(kenyaAmericano);
+		kenyaLatte.brewing();
+		System.out.println();
+		
+		Mocha kenyaMocha = new Mocha(new Latte(kenyaAmericano));
+		kenyaMocha.brewing();
+		System.out.println();
+		
+		WhippedCream etiopiaAmericano = 
+				new WhippedCream(new Mocha(new Latte(new EtiopiaAmericano())));
+		etiopiaAmericano.brewing();
+		System.out.println();
+	}
+}
---
+++ Chap20(자바IO)/src/kr/co/ezenac/decorator/EtiopiaAmericano.java
@@ -0,0 +1,14 @@
+package kr.co.ezenac.decorator;
+
+public class EtiopiaAmericano extends Coffee {
+
+	public EtiopiaAmericano() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void brewing() {
+		System.out.print("EtiopiaAmericano ");
+	}
+
+}
